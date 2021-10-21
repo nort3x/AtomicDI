eb3f4385e979a4ee905101080360cc08623e60dd
Human Ardaki <nort3x911@protonmail.com>
2021-10-21 23:50:12 +0330

    switching to slf4j
    Signed-off-by: Human Ardaki <nort3x911@protonmail.com>

--- src/main/java/me/nort3x/atomic/logger/AtomicLogger.java
+++ src/main/java/me/nort3x/atomic/logger/AtomicLogger.java
@@ -8,10 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
 
-import java.io.OutputStream;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.io.*;
+import java.util.ServiceLoader;
 import java.util.function.Supplier;
 
 public class AtomicLogger implements ILoggerFactory {
@@ -20,15 +18,12 @@ public class AtomicLogger implements ILoggerFactory {
     public static String banner =
             Color.color(
                     "      .o.           .                                o8o            oooooooooo.   ooooo \n" +
-                    "     .888.        .o8                                `\"'            `888'   `Y8b  `888' \n" +
-                    "    .8\"888.     .o888oo  .ooooo.  ooo. .oo.  .oo.   oooo   .ooooo.   888      888  888  \n" +
-                    "   .8' `888.      888   d88' `88b `888P\"Y88bP\"Y88b  `888  d88' `\"Y8  888      888  888  \n" +
-                    "  .88ooo8888.     888   888   888  888   888   888   888  888        888      888  888  \n" +
-                    " .8'     `888.    888 . 888   888  888   888   888   888  888   .o8  888     d88'  888  \n" +
-                    "o88o     o8888o   \"888\" `Y8bod8P' o888o o888o o888o o888o `Y8bod8P' o888bood8P'   o888o \n", Color.ANSI_CYAN) + "\n\n" + Color.color(Quotes.getOne(), Color.ANSI_YELLOW) + "\n\n";
-
-
-
+                            "     .888.        .o8                                `\"'            `888'   `Y8b  `888' \n" +
+                            "    .8\"888.     .o888oo  .ooooo.  ooo. .oo.  .oo.   oooo   .ooooo.   888      888  888  \n" +
+                            "   .8' `888.      888   d88' `88b `888P\"Y88bP\"Y88b  `888  d88' `\"Y8  888      888  888  \n" +
+                            "  .88ooo8888.     888   888   888  888   888   888   888  888        888      888  888  \n" +
+                            " .8'     `888.    888 . 888   888  888   888   888   888  888   .o8  888     d88'  888  \n" +
+                            "o88o     o8888o   \"888\" `Y8bod8P' o888o o888o o888o o888o `Y8bod8P' o888bood8P'   o888o \n", Color.ANSI_CYAN) + "\n\n" + Color.color(Quotes.getOne(), Color.ANSI_YELLOW) + "\n\n";
 
 
     private static OutputStream outputStream = System.out;
@@ -43,7 +38,7 @@ public class AtomicLogger implements ILoggerFactory {
     };
 
     private AtomicLogger() {
-        if(!silence) {
+        if (!silence) {
             System.out.print(banner);
         }
         Quotes.quotes = null;
@@ -80,43 +75,53 @@ public class AtomicLogger implements ILoggerFactory {
 
     @Override
     public Logger getLogger(String name) {
-        return loggerProvider(name,Priority.DEBUG) ;
+        return loggerProvider(name, Priority.DEBUG);
     }
 
-    public Logger getLogger(Class<?> c,Priority p) {
-        return loggerProvider((c.isAnonymousClass() ? "AnonymousType - " + c.getName() : c.getSimpleName()),p);
+    public Logger getLogger(Class<?> c, Priority p) {
+        return loggerProvider((c.isAnonymousClass() ? "AnonymousType - " + c.getName() : c.getSimpleName()), p);
     }
+
     public Logger getLogger(Class<?> c) {
-        return loggerProvider((c.isAnonymousClass() ? "AnonymousType - " + c.getName() : c.getSimpleName()),Priority.DEBUG);
+        return loggerProvider((c.isAnonymousClass() ? "AnonymousType - " + c.getName() : c.getSimpleName()), Priority.DEBUG);
     }
 
-    public Logger getLogger(String name,Priority p) {
-        return loggerProvider(name,p) ;
+    public Logger getLogger(String name, Priority p) {
+        return loggerProvider(name, p);
     }
 
-    private Logger loggerProvider(String name,Priority p){
-        if(silence)
+    private Logger loggerProvider(String name, Priority p) {
+        if (silence)
             return new LoggerAdaptor(); // total nop
+        if (useEmbeddedLogger)
+            new BasicLogger(new PrintStream(outputStream), p, name);
 
-       return useEmbeddedLogger ?
-               new BasicLogger(new PrintStream(outputStream),p,name) :
-               factory.getLogger(name);
+        try {
+            if (ClassLoader.getSystemResources("org/slf4j/impl/StaticLoggerBinder.class").hasMoreElements())
+                factory.getLogger(name);
+        } catch (IOException ignored) {
+
+        }
+        return new BasicLogger(new PrintStream(outputStream), p, name);
     }
 
-    public static void setSilent(boolean silence){
+    public static void setSilent(boolean silence) {
         AtomicLogger.silence = silence;
     }
+
     public static void setBanner(String banner) {
         AtomicLogger.banner = banner;
     }
-    public static void setQuote(String quote){
+
+    public static void setQuote(String quote) {
         AtomicLogger.quote = quote;
     }
-    public static void setForceUseEmbeddedLogger(boolean useEmbeddedLogger){
+
+    public static void setForceUseEmbeddedLogger(boolean useEmbeddedLogger) {
         AtomicLogger.useEmbeddedLogger = useEmbeddedLogger;
     }
 
-    public static void useCustomLogger(ILoggerFactory loggerFactory){
+    public static void useCustomLogger(ILoggerFactory loggerFactory) {
         AtomicLogger.factory = loggerFactory;
     }
 
