6b59544f5b9ea4b5b5418dd39d757b692e93b477
Human Ardaki <nort3x911@protonmail.com>
2021-11-30 13:53:31 +0330

    env variables now override predefined values
    Signed-off-by: Human Ardaki <nort3x911@protonmail.com>

--- src/main/java/me/nort3x/atomic/core/integrator/PredefinedLoader.java
+++ src/main/java/me/nort3x/atomic/core/integrator/PredefinedLoader.java
@@ -8,36 +8,40 @@ import org.slf4j.Logger;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Locale;
 import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class PredefinedLoader {
 
 
-    private static final ConcurrentHashMap<String,Value> values = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<String, Value> values = new ConcurrentHashMap<>();
 
 
     public static void addDefinitionFile(InputStream inputStream) throws IOException {
         Properties p = new Properties();
         p.load(inputStream);
-        p.entrySet().forEach(entry->{
-            values.put(entry.getKey().toString(),new Value(entry.getKey().toString(),entry.getValue().toString()));
+        p.entrySet().forEach(entry -> {
+            values.put(entry.getKey().toString(), new Value(entry.getKey().toString(), entry.getValue().toString()));
         });
     }
 
-    private static Logger l = AtomicLogger.getInstance().getLogger(PredefinedLoader.class,Priority.IMPORTANT);
+    private static Logger l = AtomicLogger.getInstance().getLogger(PredefinedLoader.class, Priority.IMPORTANT);
 
     public static void setPredefinedValue(AtomicField f, Object obj) {
         if (!f.isPredefined()) {
             l.warn("Predefined field Injector is called for AtomicField: " + f.getCorrespondingField().getDeclaringClass().getName() + "." + f.getCorrespondingField().getName() + "but its not annotated with Predefined");
         }
         String key = f.getCorrespondingField().getAnnotation(Predefined.class).value();
-        Value value = values.getOrDefault(key,null);
+        String value_env = System.getenv(key.toUpperCase(Locale.ROOT).replace(".", "_"));
+        Value value = value_env == null ? null : new Value(key, value_env);
+        if (value == null)
+            value = values.getOrDefault(key, null);
         if (value == null) {
             l.warn("Predefined key-value: " + f.getCorrespondingField().getDeclaringClass().getName() + "." + f.getCorrespondingField().getName() + " is [not Found] among scanned entry paths");
             return;
         }
-        f.setField(obj,value.readFor(f.getType()));
+        f.setField(obj, value.readFor(f.getType()));
     }
 
 
